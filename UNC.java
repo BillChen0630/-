d77e1fe0a442c5d673cc85082d07d4cb29048d1a
MohanChen <64602313+BillChen0630@users.noreply.github.com>
2021-08-25 16:33:55 +0800

    Add files via upload

--- UNC.java
+++ UNC.java
@@ -21,12 +21,12 @@ class NumIn{
         }
         //定义并初始化翻译数组
         String[] Translate = {"零","壹","贰","叁","肆","伍","陆","柒","捌","玖"};
-        String[] DanWei = {"点"," ","拾","佰","仟","万","拾","百","千","亿","拾","百","千","兆"};
+        String[] DanWei = {"点"," ","拾","佰","仟","万","拾","佰","仟","亿","拾","佰","仟","兆","拾","佰","仟","京"};
         int LON = this.Num.length() - 3;
         //System.out.println(LON);
         int[] Shuzi;
         //长度为Shuzi数组的维数
-        Shuzi = new int[LON+3];
+        Shuzi = new int[LON+1];
         //解析String型变量num为int型
         double num = Double.parseDouble(this.Num);
         //定义并初始化分离数字的变量TiQu
@@ -42,35 +42,52 @@ class NumIn{
         //单位输入变量
         int b = LON;
         //记录次数的变量
-        int c = LON;
+        int c = 0;
+        //判别是否加单位的变量
+        int p = LON;
         //设定一个局部变量保存翻译的东西
         double djbbl;
         int jbbl;
         //设置是否显示零的boolean变量
         boolean dui = true;
-        for(int j = LON ; j>0 ; j--){
+        //解析数字
+        for(int m = 0 ; m<LON ; m++){
             djbbl = num / TiQu;
             jbbl = (int)djbbl;
-            Shuzi[c] = jbbl;
-            if(jbbl == 0){
-                if(Shuzi[c+1] == 0 || c%4 == 1){
+            Shuzi[m] = jbbl;
+            if(TiQu != 1){
+            num -= TiQu * jbbl;
+            }else{
+            num -= jbbl ;
+            }
+            TiQu /= 10;
+        }
+        for(int j = 0; j<LON ; j++){
+            if(Shuzi[c] == 0){
+                if(Shuzi[c+1] == 0 || b%4 == 1){
                     dui = false;
-                }
+                }else{dui = true;}
             }else{dui = true;}
             if(dui){
-            OutArr[a] = Translate[jbbl];
+            OutArr[a] = Translate[Shuzi[c]];
             a ++;
             }
-            if(jbbl != 0 || c%4 == 1){
+            if(Shuzi[c] != 0 || b%4 == 1){
             OutArr[a] = DanWei[b];
             a ++;
             }
+            if(Shuzi[0] == 0){
+                 OutArr[0] = Translate[0];
+            a +=1;
+            break;
+            }
+            if(b%4 == 1 && Shuzi[c] == 0){
+                if(Shuzi[c-3]+Shuzi[c-2]+Shuzi[c-1]+Shuzi[c] == 0)
+                a--;
+            }
             b--;
-            if(TiQu != 1){
-            num -= TiQu * jbbl;}else{num -= jbbl ;}
-            TiQu /= 10;
-            c--;
-        }
+            c++;
+            }
         //计算整数所占位数
         this.zheng = a-1;
         num +=0.001;
